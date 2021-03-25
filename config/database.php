ef0ef82800fa3eaa28385953649b5baac6236dc0
Celestine <decele2011@gmail.com>
2021-03-26 00:29:50 +0100

    database is

--- config/database.php
+++ config/database.php
@@ -58,9 +58,15 @@ return [
             'username' => $username,
             'password' => $password,
             'port' => env('DB_PORT', '3306'),
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
         ],
 
         'mysql' => [
